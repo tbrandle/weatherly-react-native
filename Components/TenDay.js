568ec824676aed4a5b65844af702c7ea576c8ae6
Tim <tbrandle53@gmail.com>
2017-08-16 11:26:56 -0600

    Add tenday component and centered alignment

--- Components/TenDay.js
+++ Components/TenDay.js
@@ -5,15 +5,15 @@ const TenDay = ({ tenDayArray }) => {
 
     return (
       <View style={styles.tenDaySection}>
-        <Text>Ten Day Forecast</Text>
+        <Text style={{textAlign: 'center'}} >Ten Day Forecast</Text>
         { tenDayArray.map((value, i) => {
           return (
-            <View style={styles.tenDayForescast}>
-              <Text >
+            <View style={styles.tenDayForecast}>
+              <Text style={{width: 87}}>
                 { tenDayArray[i].day }
               </Text>
-              <Image source={{uri: tenDayArray[i].icon }} />
-              <Text>
+              <Image style={styles.weatherIcon} source={{uri: tenDayArray[i].icon }} />
+              <Text style={{width: 87}}>
                 { tenDayArray[i].high }&deg; / { tenDayArray[i].low }&deg;
               </Text>
             </View>
@@ -30,10 +30,18 @@ const styles = StyleSheet.create({
     marginVertical: 10,
   },
   tenDayForecast: {
-    flexWrap: 'wrap',
-    borderBottomWidth: 2,
-    borderBottomColor: 'black',
-    marginBottom: 5,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    flexDirection: 'row',
+    borderBottomWidth: 1,
+    borderBottomColor: 'lightgrey',
+    margin: 5,
+    paddingBottom: 13,
+    paddingHorizontal: 5,
+  },
+  weatherIcon:{
+    height: 35,
+    width: 35,
   },
 })
 
